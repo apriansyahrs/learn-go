26b4b054c1de2f87e9fe1dfafefbc9e6874402f4
apriansyahrs <apriansyahrizs@gmail.com>
2022-07-21 10:58:50 +0700

    Package fmt

--- basic.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-	// Cetak "Hello, world"
-	println("Hello, world")
-
-	// Baris ini seharusnya merupakan sebuah komentar
-
-}
--- format.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func main() {
-	println("Hello, world")
-}
-func sub() {
-	println("Hello, Go")
-}
